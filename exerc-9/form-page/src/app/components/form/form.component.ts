94cf331a12860c8236b87ea6bbbe86cb1b777533
thiagotfsilva <thiagophsilva@hotmail.com>
2025-04-17 12:48:18 -0300

    feat: adicionando validacao no formulario do exercicio 9

--- exerc-9/form-page/src/app/components/form/form.component.ts
+++ exerc-9/form-page/src/app/components/form/form.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent {
-  formGroup = new FormGroup({
-    nome: new FormControl(""),
-    modelo: new FormControl(1)
-  });
+export class FormComponent implements OnInit {
+  contatoForm!: FormGroup;
+
+  formInit() {
+    this.contatoForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.maxLength(300), Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      estado: new FormControl('', Validators.required),
+      cidade: new FormControl('', Validators.required),
+      instituicao: new FormControl('', [Validators.required, Validators.maxLength(300), Validators.minLength(3)])
+    });
+  }
+
+  ngOnInit() {
+    this.formInit();
+  }
+
+  onSubmit() {
+    if (this.contatoForm.valid) {
+      console.log(this.contatoForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
 }
